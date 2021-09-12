094970154c71c57f7a0fd5807e758c0b0a06d0ec
Jonatancon <jhonatanlora2011@gmail.com>
2021-09-12 17:05:45 -0500

    initial

---
+++ src/main/java/com/pragma/reddeapoyointernacionalbackend/RedDeApoyoInternacionalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.reddeapoyointernacionalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedDeApoyoInternacionalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedDeApoyoInternacionalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/reddeapoyointernacionalbackend/RedDeApoyoInternacionalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.reddeapoyointernacionalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedDeApoyoInternacionalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
