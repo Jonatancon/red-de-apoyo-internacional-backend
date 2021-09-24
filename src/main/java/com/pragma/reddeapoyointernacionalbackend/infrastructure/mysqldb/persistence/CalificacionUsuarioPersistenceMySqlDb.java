3bd48c5e81fb412c0ee03cbe9ec7367ec4e19cf5
Jonatancon <jhonatanlora2011@gmail.com>
2021-09-24 15:16:20 -0500

    Corregido bug en la clase UsuarioPersistenceMySqlDb #3

--- src/main/java/com/pragma/reddeapoyointernacionalbackend/infrastructure/mysqldb/persistence/CalificacionUsuarioPersistenceMySqlDb.java
+++ src/main/java/com/pragma/reddeapoyointernacionalbackend/infrastructure/mysqldb/persistence/CalificacionUsuarioPersistenceMySqlDb.java
@@ -1,10 +1,7 @@
 package com.pragma.reddeapoyointernacionalbackend.infrastructure.mysqldb.persistence;
 
-import com.pragma.reddeapoyointernacionalbackend.domain.model.CalificacionUsuario;
 import com.pragma.reddeapoyointernacionalbackend.domain.persistence.CalificacionUsuarioPersistence;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class CalificacionUsuarioPersistenceMySqlDb implements CalificacionUsuarioPersistence {
