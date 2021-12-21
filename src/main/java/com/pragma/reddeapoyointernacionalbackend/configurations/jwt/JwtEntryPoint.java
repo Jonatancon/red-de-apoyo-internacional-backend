bcda5f95409f421a6c1d186351eff36b3bb86ce1
Jonatancon <jhonatanlora2011@gmail.com>
2021-12-21 11:49:14 -0500

    solucionado el problema con las restricciones a los end-point #10

--- src/main/java/com/pragma/reddeapoyointernacionalbackend/configurations/jwt/JwtEntryPoint.java
+++ src/main/java/com/pragma/reddeapoyointernacionalbackend/configurations/jwt/JwtEntryPoint.java
@@ -1,12 +1,15 @@
 package com.pragma.reddeapoyointernacionalbackend.configurations.jwt;
 
+import com.pragma.reddeapoyointernacionalbackend.http_errors.RequestErrors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @Slf4j
@@ -14,8 +17,9 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException e){
+                         AuthenticationException e) throws IOException {
 
-        log.error("error en el metodo commons");
+        log.error("error en el metodo commons " + e.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
     }
 }
